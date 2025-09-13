5fe69caf342ef95fb24ed1c384de24f0ed089a04
rahulbhardwaj98 <36204403-rahulbhardwaj98@users.noreply.replit.com>
2025-09-13 15:35:10 +0000

    Initial commit

---
+++ pages/components/queryform.ts
@@ -0,0 +1,15 @@
+"use client";
+
+function queryRequest(){
+  console.log("Query Requested ! !")
+}
+
+export default function QueryForm(){
+
+  return(
+    <div>
+      <h1>Query Form</h1>
+    </div>
+  )
+    
+}
